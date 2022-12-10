f88ad7a9f6992a245dc894b5ad516d321b9e8492
Spencer Hansen <spencerhansen@gmail.com>
2022-12-10 09:44:45 -0700

    module 2 exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+using System.Transactions;
+
+string firstName = "Spencer";
+char lastNameInitial = 'H';
+decimal currentWeight = 198.8m;
+int desiredWeightClass = 170;
+double amountToLose = 28.8;
+bool possibility = true;
+
+Console.WriteLine($"{firstName}, {lastNameInitial}");
+Console.WriteLine($"Has a current weigth of{currentWeight}lbs");
+Console.WriteLine($"Weight goal of {desiredWeightClass}lbs");
+Console.WriteLine($"Need to lose {amountToLose} lbs");
+Console.WriteLine($"I this a good weight goal? true=yes false=no");
+Console.WriteLine($"{possibility}");
